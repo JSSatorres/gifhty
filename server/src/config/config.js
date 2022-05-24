55cece1203e88edebfae513a19e1232c773bf8c0
JSSatorres <juansataz@gmail.com>
2022-05-24 09:54:51 +0200

    conect DB ok

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,30 +2,30 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const MONGO_URI = process.env.MONGO_URI || "htpp:atlas.test";
+export const MONGO_URL = process.env.MONGODB_URL || "htpp:atlas.test";
 export const PORT = process.env.PORT || 4000;
-const CONFIG = {
-  db: {
-    url: process.env.DB_URL,
-  },
-  firebase: {
-    certConfig: {
-      type: process.env.type,
-      project_id: process.env.project_id,
-      private_key_id: process.env.private_key_id,
-      private_key: process.env.private_key,
-      client_email: process.env.client_email,
-      client_id: process.env.client_id,
-      auth_uri: process.env.auth_uri,
-      token_uri: process.env.token_uri,
-      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-      client_x509_cert_url: process.env.client_x509_cert_url,
-    },
-  },
+// const CONFIG = {
+//   db: {
+//     url: process.env.DB_URL,
+//   },
+//   firebase: {
+//     certConfig: {
+//       type: process.env.type,
+//       project_id: process.env.project_id,
+//       private_key_id: process.env.private_key_id,
+//       private_key: process.env.private_key,
+//       client_email: process.env.client_email,
+//       client_id: process.env.client_id,
+//       auth_uri: process.env.auth_uri,
+//       token_uri: process.env.token_uri,
+//       auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+//       client_x509_cert_url: process.env.client_x509_cert_url,
+//     },
+//   },
 
-  cloudinary: {
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret,
-  },
-};
+//   cloudinary: {
+//     cloud_name: process.env.cloud_name,
+//     api_key: process.env.api_key,
+//     api_secret: process.env.api_secret,
+//   },
+// };
--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,23 +1,12 @@
 import mongoose from "mongoose";
-
-// const URI = process.env.MONGODB_URL;
-const URI = "mongodb+srv://JSS:CaraVaya2020@cluster0.hww2k.mongodb.net/GiftDB?retryWrites=true&w=majority"
-
-// export default function connectDB() {
-//   return mongoose.connect(URI);
-// }
+import { MONGO_URL } from "../config/config.js";
 
 
 export default async function connectDB (){
   try{
-      const db = await mongoose.connect( URI)
+      const db = await mongoose.connect( MONGO_URL)
       console.log("conecto to db ", db.connection.name );
-      // console.log("conecto to db ");
   }catch(error){
       console.error(error);
   }
 }
-
-// export default function connectDB() {
-//   return mongoose.connect(URI);
-// }
