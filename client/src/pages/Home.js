aac6b89f66a66b2da04577292f3cc577353f82e0
JSSatorres <juansataz@gmail.com>
2022-06-03 07:36:20 +0200

    front environment variables

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import GiftList from "../components/giftList";
 import Input from "../components/input";
 import Navbar from "../components/navbar";
@@ -6,9 +6,9 @@ import { useAuth } from "../context/AuthContext";
 
 const Home = () => {
   const [keyword, setKeyword] = useState();
-const isUserLogin = useAuth()
-console.log(isUserLogin)
+  const isUserLogin = useAuth();
 
+  console.log(isUserLogin);
 
   function keywordSeacrh(newKeyword) {
     setKeyword(newKeyword);
@@ -17,6 +17,7 @@ console.log(isUserLogin)
   return (
     <section className="bg-secondary">
       <Navbar />
+
       <Input keywordSeacrh={keywordSeacrh} />
       <div className="mx-5">
         <GiftList keyword={keyword} />
