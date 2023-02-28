eea8b16e9a62358b334a14f099f3af41201626fb
jssatorres <juansataz.dev@gmail.com>
2023-02-28 13:32:35 +0100

    fix (main) add enviroment variable

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js"
-// import {PORT} from "./config/config.js"
+import {PORT} from "./config/config.js"
 import connectDB from "./db/connect.js"
 
 connectDB()
 
-app.listen(4000, ()=>console.log("listening",4000))
+app.listen(PORT, ()=>console.log("listening",PORT))
