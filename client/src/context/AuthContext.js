0bb2dc9b634e540639a6a9673b33ea54edeb2dec
jssatorres <juansataz.dev@gmail.com>
2023-02-28 14:04:36 +0100

    Fix (firebase) adding variable

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,29 +1,29 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import React from "react";
-import {auth} from "../firebae/firebase"
+import {auth} from "../firebase/firebase"
 import { onAuthStateChanged } from "firebase/auth";
 
-//create the context
-export const AuthContext = createContext(null);
+const AuthContext = createContext(null);
 
-//custom hook who is the imported hook in the ohter components
+//custom hook 
 export const useAuth = () => {
   const user = useContext(AuthContext);
   return user;
 };
 
-// the provider to consume the value which embraces the other components
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     onAuthStateChanged(auth, currentUser => {
+      setLoading(true)
       setUser(currentUser)
       setLoading(false)
       console.log(loading);
       console.log(user);
-    })}, []);
+    })}, [user]);
 
 
   return (
---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,90 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  // GoogleAuthProvider,
+  // signInWithPopup,
+  signOut,
+  // sendPasswordResetEmail,
+  // updatePassword,
+  // reauthenticateWithCredential,
+  // EmailAuthProvider,
+} from "firebase/auth";
+
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP__AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId:process.env.REACT_APP_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+
+export async function singInWithEmailAndPassword(email, password) {
+  await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function singUpWithEmailAndPassword(email, password) {
+  await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function userSignOut() {
+  return await signOut(auth);
+}
+// export function singInWithGoogle() {
+//   const Provider = new GoogleAuthProvider();
+//   return signInWithPopup(auth, Provider);
+// }
+
+// export function userSendPasswordResetEmail(email) {
+//   return sendPasswordResetEmail(auth, email);
+// }
+
+// export function getCurrentUserToken() {
+//   if (!auth.currentUser) {
+//     return null;
+//   }
+
+//   return auth.currentUser.getIdToken();
+// }
+
+// export function getCurrentUserEmail() {
+//   if (!auth.currentUser) {
+//     return null;
+//   }
+
+//   return auth.currentUser.email;
+// }
+
+// export function changePassword(newPassword) {
+//   const auth = getAuth();
+
+//   const user = auth.currentUser;
+//   console.log(user);
+//   console.log(newPassword);
+//   updatePassword(user, newPassword)
+//     .then(() => console.log("contraseña cambiada"))
+//     .catch(() => console.log("contraseña no cambiada"));
+// }
+
+// export async function reauthenticate(password) {
+//   const user = auth.currentUser;
+//   const credential = EmailAuthProvider.credential(
+//     auth.currentUser.email,
+//     password
+//   );
+//   return reauthenticateWithCredential(user, credential);
+// }
