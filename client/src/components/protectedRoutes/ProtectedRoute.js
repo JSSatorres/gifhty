11fa4837e56b4a72b35774cdb3ec7b3edc7c2eeb
jssatorres <juansataz.dev@gmail.com>
2023-03-02 17:18:09 +0100

    Fix (Private Route) cleaning code

--- client/src/components/protectedRoutes/ProtectedRoute.js
+++ client/src/components/protectedRoutes/ProtectedRoute.js
@@ -1,16 +1,13 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
 
 const ProtectedRoute = ({ children }) => {
-  const { user, loading } = useAuth();
-  if (loading) return <h1>loading</h1>;
-  console.log(user);
+  const { user, loading } = useAuth()
+  if (loading) return <h1>loading</h1>
+  if (!user) return <Navigate to="/" />
+  return <Outlet />
 
-  if (!user) return <Navigate to="/" />;
+}
 
-  return <Outlet />;
-
-};
-
-export default ProtectedRoute;
+export default ProtectedRoute
