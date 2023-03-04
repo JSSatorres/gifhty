6326308b0d45f80f8ac59274c3f5dcce3ff5290e
jssatorres <juansataz.dev@gmail.com>
2023-03-04 09:26:45 +0100

    Fix (Navbar) delete collapse bar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,72 +1,72 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-import { userSignOut } from "../../firebae/firebase";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
+import { userSignOut } from '../../firebase/firebase'
+import {  FiLogOut } from 'react-icons/fi'
+import { AiFillHome } from 'react-icons/ai'
 
 const Navbar = () => {
-  const { user } = useAuth();
+  const { user } = useAuth()
 
   function handelSignOut() {
-    userSignOut();
+    userSignOut()
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div className="container-fluid border-bottom border-warning">
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container-fluid border-bottom border-warning ">
         {user ? (
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <Link to="/" className="nav-link">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                height="15"
-                alt="MDB Logo"
-                loading="lazy"
-              />
+          <div className="d-flex justify-content-around w-100" id="navbarSupportedContent">
+            <Link to="/" className="nav-link text-warning ">
+              <span style={{fontSize:'20px'}}><AiFillHome /></span>              
             </Link>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+            <ul className="navbar-nav w-100 mb-2 mb-lg-0 d-flex flex-row justify-content-center">
+              <li className="nav-item mx-2">
                 <Link to="/profile" className="nav-link text-warning">
                   Profile
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item mx-2">
                 <Link to="/upload" className="nav-link text-warning me-auto">
                   Upload
                 </Link>
               </li>
-              <li className="nav-item ">
+              <li className="nav-item  mx-2">
                 <Link to="/mygifts" className="nav-link text-warning ">
                   my Gifts
                 </Link>
               </li>
             </ul>
-            <div className="nav-item ms-auto">
+            <div className="nav-item ">
               <Link
                 onClick={handelSignOut}
                 to="/"
-                className="nav-link text-warning"
-              >
-                Sign out
+                className="nav-link text-warning d-flex flex-row"
+              > Exit
+                <span className='mx-1'><FiLogOut /></span>                
               </Link>
             </div>
           </div>
         ) : (
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <Link to="/" className="nav-link">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                height="15"
-                alt="MDB Logo"
-                loading="lazy"
-              />
-            </Link>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+          <div className="" id="navbarSupportedContent">
+  
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row">
+              <li className="nav-item mx-2">
+                <Link to="/" className="nav-link">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+                    height="15"
+                    alt="MDB Logo"
+                    loading="lazy"
+                  />
+                </Link>
+              </li>
+              <li className="nav-item mx-2">
                 <Link to="/register" className="nav-link text-warning">
                   Register
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item mx-2">
                 <Link to="/login" className="nav-link text-warning">
                   Login
                 </Link>
@@ -76,7 +76,7 @@ const Navbar = () => {
         )}
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
