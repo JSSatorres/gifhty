bdf701470b500d06463b186b66a7d65896306608
jssatorres <juansataz.dev@gmail.com>
2023-03-06 19:26:08 +0100

    Fix (GiftCard) clean code

--- client/src/components/giftCard/GiftCard.js
+++ client/src/components/giftCard/GiftCard.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
 function GiftCard({ title, id, url }) {
   return (
     <div className="card text-white bg-dark mb-3 col-md-3  px-1 mx-5 border border-warning rounded-5" key={id}>
       <div className="card-header">{title}</div>
       <div className="card-body">
-        <img class="rounded mx-auto my-auto d-block img-fluid" src={url} alt={title} />
+        <img className="rounded mx-auto my-auto d-block img-fluid" src={url} alt={title} />
       </div>
     </div>
-  );
+  )
 }
 
-export default GiftCard;
+export default GiftCard
