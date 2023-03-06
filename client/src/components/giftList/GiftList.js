840c69db4e5cfa6c4530c25d932939d32f4c68c5
jssatorres <juansataz.dev@gmail.com>
2023-03-06 19:26:22 +0100

    Fix (GiftList) clean code

--- client/src/components/giftList/GiftList.js
+++ client/src/components/giftList/GiftList.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
-import apiGiphy from "../../servicesExt/apiGiphy";
-import GiftCard from "../giftCard/GiftCard";
+import React, { useEffect, useState } from 'react'
+import apiGiphy from '../../servicesExt/apiGiphy'
+import GiftCard from '../giftCard/GiftCard'
 
 const GiftList = ({keyword}) => {
-  const [gifs, setGifs] = useState();
+  const [gifs, setGifs] = useState()
 
   useEffect(() => {
     apiGiphy(keyword).then((apiGifts) => {
-      console.log(apiGifts);
-      setGifs(apiGifts);
-    });
-  }, [keyword]);
+      console.log(apiGifts)
+      setGifs(apiGifts)
+    })
+  }, [keyword])
   return (
     <section className="row justify-content-center">
       {gifs
         ? gifs.map((singleGift) => {
-            return (
-              <GiftCard
-                key={singleGift.id}
-                title={singleGift.title}
-                id={singleGift.id}
-                url={singleGift.url}
-              />
-            );
-          })
+          return (
+            <GiftCard
+              key={singleGift.id}
+              title={singleGift.title}
+              id={singleGift.id}
+              url={singleGift.url}
+            />
+          )
+        })
         : null}
     </section>
-  );
-};
+  )
+}
 
-export default GiftList;
+export default GiftList
