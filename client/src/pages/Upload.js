0094f2f5e03b784701d20f23fe89fa0ecee389f0
jssatorres <juansataz.dev@gmail.com>
2023-03-06 19:27:36 +0100

    Fix (Upload) add gif to DB

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,74 +1,98 @@
-import { useState, useCallback } from "react";
-import { Formik, Form, Field } from "formik";
-import Navbar from "../components/navbar";
-import { useDropzone } from "react-dropzone";
-import { useCreateGiftMutation, useGetGiftsQuery } from "../services/giftApi";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useCallback } from 'react'
+import { Formik } from 'formik'
+import Navbar from '../components/navbar'
+import { useDropzone } from 'react-dropzone'
+import { useCreateGiftMutation, useGetGiftsQuery } from '../services/giftApi'
+import { Link, useNavigate } from 'react-router-dom'
+import uploadSchema from './yupSchemas/UploadSchema copy'
 
 function Upload() {
-  const [image, setImage] = useState([]);
-  const [createGift] = useCreateGiftMutation();
-  const navigate = useNavigate();
-  const {  refetch } = useGetGiftsQuery();
+  const [image, setImage] = useState([])
+  const [createGift] = useCreateGiftMutation()
+  // const navigate = useNavigate()
+  // const {  refetch } = useGetGiftsQuery()
 
   const onDrop = useCallback((acceptedFiles, rejectFiles) => {
     acceptedFiles.forEach((file) => {
-      const reader = new FileReader();
+      const reader = new FileReader()
       reader.onload = () => {
-        setImage((prevState) => [...prevState, reader.result]);
-      };
-      reader.readAsDataURL(file);
-      console.log("el reader", reader);
-    });
+        setImage((prevState) => [...prevState, reader.result])
+      }
+      reader.readAsDataURL(file)
+      console.log('el reader', reader)
+      console.log('el image', image)
+    })
 
-    console.log("acceptedFiles", acceptedFiles);
-    console.log("rejectFiles", rejectFiles);
-  }, []);
+    console.log('acceptedFiles', acceptedFiles)
+    console.log('rejectFiles', rejectFiles)
+  }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
-    accept: {
-      "image/*": [".jpeg", ".png", ".gif"],
-    },
-  });
-
+    accept:''
+  })
+  const handleSubmit  =  (values) => {
+    console.log(values,image)
+    createGift(values,image)
+  }
   return (
-    <div className="bg-secondary vh-100">
-      <Navbar />
-      <div className="container  mt-5 pt-5 w-100">
-        <Formik
-          initialValues={{
-            title: "",
-            gift: "",
-            user: "",
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            createGift(values);
-            navigate(`/mygifts`);
-            refetch();
-          }}
-        >
-          {({ isSubmitting }) => (
-            <div className="container te">
-              <div className="card-body mt-5 pt-5 bg-dark">
-                <Form>
-                  <h4 className="text-white text-center my-2">
-                    Upload your Gift
-                  </h4>
-                  <div className="d-flex flex-column">
-                    <label htmlFor="title" className="text-white my-3">
-                      title
-                    </label>
-                    <Field type="text" name="title" />
-                    <label htmlFor="gift" className="text-white my-3">
-                      gift
-                    </label>
-                    <Field type="text" name="gift" />
-                    <label htmlFor="user" className="text-white my-3">
-                      user
-                    </label>
-                    <Field type="text" name="user" />
+    <section className='container-fluid py-4 h-100 bg-secondary'>
+      <Formik
+        initialValues={{ title: '', url: ''}}
+        validationSchema={uploadSchema}
+        onSubmit={(values) => {
+          handleSubmit(values)
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <div className='row d-flex justify-content-center align-items-center h-100'>          
+            <form className="col-12 col-md-8 col-lg-6 col-xl-5 mt-5" onSubmit={handleSubmit}>
+              <div className="card bg-dark text-warning" style={{ borderRadius: '1rem' }}>
+                <div className="card-body px-5  text-center">
+                  <div className="mb-md-3 mt-md-2 pb-3">
+                    <p className="text-warning-50 mb-3">
+                      Enter url and title of the gift to be uploaded
+                    </p>
+                    <div className="form-outline form-white mb-3">
+                      <label className="form-label text-warning" htmlFor="typeEmailX">Title</label>
+                      <input
+                        type="text"
+                        id="typetitle"
+                        className="form-control form-control-lg bg-secondary  btn-outline-warning"
+                        name='title'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.title}
+                        placeholder='your user name'
+                      />
+                      {errors.title && touched.title && errors.title
+                        ? <div className='text-danger mt-2'>{errors.title}</div>
+                        : null}
+                    </div>
+                    <div className="form-outline form-white mb-4">
+                      <label className="form-label text-warning" htmlFor="typeEmailX">Url</label>
+                      <input
+                        type="url"
+                        id="typeEmailX"
+                        className="form-control form-control-lg bg-secondary  btn-outline-warning"
+                        name='url'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.url}
+                        placeholder='name@company.com'
+                      />
+                      {errors.url && touched.url && errors.url
+                        ? <div className='text-danger mt-2'>{errors.url}</div>
+                        : null}
+                    </div>
                     <div
-                      className="dropzone text-light bg-secondary h-50 rounded mt-3 text-center py-4"
+                      className="dropzone text-warning bg-secondary h-50 rounded mt-3 text-center py-4 my-3 btn-outline-warning"
                       {...getRootProps()}
                     >
                       <input
@@ -77,25 +101,31 @@ function Upload() {
                         placeholder="as"
                         {...getInputProps()}
                       />
-                      {isDragActive ? "Drag active" : "You can drop your files"}
+                      {isDragActive ? 'Drag active' : 'You can drop your files'}
                     </div>
-                    <div className="d-flex justify-content-center">
-                      <button
-                        type="submit"
-                        className="btn btn-warning mt-3 btn-"
-                      >
-                        Submit
-                      </button>
-                    </div>
+                    <button 
+                      className="btn btn-outline-warning  btn-md px-5" 
+                      type="submit"
+                      onMouseOver={(e) => {
+                        e.target.style.color = '#000000'
+                        e.target.style.backgroundColor = '#c79100'
+                      }}
+                      onMouseOut={(e) => {
+                        e.target.style.color = '#ffc107'
+                        e.target.style.backgroundColor = 'transparent'
+                      }}
+                    >
+                    Upload
+                    </button>
                   </div>
-                </Form>
+                </div>
               </div>
-            </div>
-          )}
-        </Formik>
-      </div>
-    </div>
-  );
+            </form>
+          </div>
+        )}
+      </Formik>
+    </section>            
+  )
 }
 
-export default Upload;
+export default Upload
