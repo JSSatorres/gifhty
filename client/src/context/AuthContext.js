87bb3e846146446a49d4d2b94af4f30ff08068e3
jssatorres <juansataz.dev@gmail.com>
2023-03-08 18:09:47 +0100

    Fix (Context¡) Promisify firebase

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,34 +1,40 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import React from "react";
-import {auth} from "../firebase/firebase"
-import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from 'react'
+import React from 'react'
+import {auth} from '../firebase/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
 
-const AuthContext = createContext(null);
+const AuthContext = createContext(null)
 
 //custom hook 
 export const useAuth = () => {
-  const user = useContext(AuthContext);
-  return user;
-};
+  const user = useContext(AuthContext)
+  return user
+}
 
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState()
   const [loading, setLoading] = useState(true)
 
+
   useEffect(() => {
-    onAuthStateChanged(auth, currentUser => {
+    const fetchData = async () => {
       setLoading(true)
-      setUser(currentUser)
+      await new Promise((resolve) => {
+        onAuthStateChanged(auth, (currentUser) => {
+          setUser(currentUser)
+          resolve()
+        })
+      })
       setLoading(false)
-      console.log(loading);
-      console.log(user);
-    })}, [user]);
+    }
+    fetchData()
+  }, [user])
 
 
   return (
     <AuthContext.Provider value={{ user,loading }}>{children}</AuthContext.Provider>
-  );
-};
+  )
+}
 
-export default AuthProvider;
+export default AuthProvider
