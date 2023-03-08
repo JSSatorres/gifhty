17b853a84d75683bcf8cb3e48219dec1b51bd1bf
jssatorres <juansataz.dev@gmail.com>
2023-03-08 18:08:22 +0100

    Fix (MyGiftList) clean code

--- client/src/components/myGiftList/MyGigtList.js
+++ client/src/components/myGiftList/MyGigtList.js
@@ -1,60 +1,60 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
   useGetGiftsQuery,
   useDeleteGiftMutation,
-} from "../../services/giftApi";
+} from '../../services/giftApi'
 
 const MyGigtList = () => {
-  const navigate = useNavigate();
-  const { data, isSuccess, refetch } = useGetGiftsQuery();
-  const [deleteGift] = useDeleteGiftMutation();
+  const navigate = useNavigate()
+  const { data, isSuccess, refetch } = useGetGiftsQuery()
+  const [deleteGift] = useDeleteGiftMutation()
 
 
   const handleDeleteOneGift = (id) => {
-    deleteGift(id);
-    refetch();
-  };
+    deleteGift(id)
+    refetch()
+  }
 
   const handleEditGift = (id) => {
-    navigate(`/editgift/${id}`);
-  };
+    navigate(`/editgift/${id}`)
+  }
 
   return (
     <div className="container mt-5  ">
       <div className="row">
         {isSuccess
           ? data.data.map((gift) => {
-              return (
-                <div key={gift._id} className="col-md-8 mt-5">
-                  <div className="card my-2 ">
-                    <img src={gift.gift} alt="avatar" />
-                    <div className="card-body">
-                      <h5 className="card-title">{gift.title}</h5>
-                      <p className="card-text"> {gift.email} </p>
-                    </div>
-                    <div className="container">
-                      <button
-                        onClick={() => handleDeleteOneGift(gift._id)}
-                        className="btn btn-warning mb-2"
-                      >
+            return (
+              <div key={gift._id} className="col-md-8 mt-5">
+                <div className="card my-2 ">
+                  <img src={gift.gift} alt="avatar" />
+                  <div className="card-body">
+                    <h5 className="card-title">{gift.title}</h5>
+                    <p className="card-text"> {gift.email} </p>
+                  </div>
+                  <div className="container">
+                    <button
+                      onClick={() => handleDeleteOneGift(gift._id)}
+                      className="btn btn-warning mb-2"
+                    >
                         Delete
-                      </button>
-                      <button
-                        className="btn btn-primary mb-2 mx-2"
-                        onClick={() => handleEditGift(gift._id)}
-                      >
+                    </button>
+                    <button
+                      className="btn btn-primary mb-2 mx-2"
+                      onClick={() => handleEditGift(gift._id)}
+                    >
                         edit
-                      </button>
-                    </div>
+                    </button>
                   </div>
                 </div>
-              );
-            })
+              </div>
+            )
+          })
           : null}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MyGigtList;
+export default MyGigtList
