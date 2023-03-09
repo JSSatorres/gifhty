8c07beb3938137f2e409b52c74d552714547c3a5
jssatorres <juansataz.dev@gmail.com>
2023-03-09 18:12:43 +0100

    feat (test) unitari test Login

---
+++ client/test/pages/Login.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Login from '../../src/pages/Login'
+
+describe('When user fills in and submits form', () => {
+  it('submit method is called with title and content', () => {
+    const submit = jest.fn()
+    render(<Login submit={submit} />)
+
+    const title = screen.getByLabelText(/title/i)
+    userEvent.type(title, 'My awesome post')
+
+    const content = screen.getByLabelText(/content/i)
+    userEvent.type(content, 'Lorem ipsum dolor sit amet')
+
+    const button = screen.getByRole('button', { name: /submit/i })
+    userEvent.click(button)
+
+    expect(submit).toHaveBeenCalledWith({
+      title: 'My awesome post',
+      content: 'Lorem ipsum dolor sit amet',
+    })
+  })
+})
