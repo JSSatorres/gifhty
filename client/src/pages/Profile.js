36a568a7f59cd47d2227f318ea51fa050abe43eb
jssatorres <juansataz.dev@gmail.com>
2023-04-15 08:40:27 +0200

    Feat (Profile) changin edit contentEditable={true}

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/navbar'
+import UiButton from '../components/ui/UiButton'
+
 const Profile = () => {
+  const [name, setName] = useState('Danny McLoan')
+  const [emil, setEmail] = useState('Danny McLoan')
+  // const [name, setName] = useState('Danny McLoan')
+  // const [name, setName] = useState('Danny McLoan')
+  const handleNombreBlur = (event) => {
+    console.log('oliii')
+    setName(event.target.textContent)
+  }
   return (
     <div>
       <Navbar />
@@ -9,49 +19,40 @@ const Profile = () => {
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col col-md-9 col-lg-7 col-xl-5">
-              <div className="card">
+              <div className="card bg-dark text-white">
                 <div className="card-body p-4">
-                  <div className="d-flex text-black col-4">
-                    <div className="flex-shrink-0 col-6">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                        alt="Generic placeholder"
-                        className="img-fluid img-thumbnail"
-                      />
-                    </div>
-                    <div className="flex-grow-1 ms-3 col-8">
-                      <h5 className="mb-1">Danny McLoan</h5>
-                      <p className="mb-2 pb-1">Senior Journalist</p>
-                      <div className="d-flex justify-content-start rounded-3 p-2 mb-2">
-                        <div>
-                          <p className="small text-muted mb-1">Articles</p>
-                          <p className="mb-0">41</p>
-                        </div>
-                        <div className="px-3">
-                          <p className="small text-muted mb-1">Followers</p>
-                          <p className="mb-0">976</p>
-                        </div>
-                        <div>
-                          <p className="small text-muted mb-1">Rating</p>
-                          <p className="mb-0">8.5</p>
-                        </div>
-                      </div>
-                      <div className="d-flex pt-1">
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary me-1 flex-grow-1"
-                        >
-                          Chat
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-primary flex-grow-1"
-                        >
-                          Follow
-                        </button>
-                      </div>
+                  <div className="flex-grow-1 ms-3 col-8">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                      alt="Generic placeholder"
+                      className="img-fluid img-thumbnail"
+                    />
+                    <h5 
+                      className="my-2 "
+                      contentEditable={true}
+                      onBlur={handleNombreBlur}
+                    >
+                      {name}
+                    </h5>
+                    <p
+                      className="my-1 pb-1"
+                      contentEditable={true}
+                      onBlur={handleNombreBlur}
+                    >
+                      Senior Journalist
+                    </p>
+
+                    <div className="d-flex pt-1">
+                      <UiButton  func={handleNombreBlur}>
+                          Edit
+                      </UiButton>
+                      <UiButton  func={handleNombreBlur}>
+                          Save
+                      </UiButton>
+                       
                     </div>
                   </div>
+
                 </div>
               </div>
             </div>
