227fec792aa19bfdd4b95a6afe7a1874d658ed9e
jssatorres <juansataz.dev@gmail.com>
2023-04-22 19:01:00 +0200

    feat (register) unit test WIP

--- client/test/pages/Register.test.js
+++ client/test/pages/Register.test.js
@@ -1,26 +1,26 @@
-/* eslint-disable no-undef */
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Register from '../../src/pages/Register'
+// /* eslint-disable no-undef */
+// import React from 'react'
+// import { render, screen } from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
+// import Register from '../../src/pages/Register'
 
-describe('When user fills in and submits form', () => {
-  it('submit method is called with title and content', () => {
-    const submit = jest.fn()
-    render(<Register submit={submit} />)
+// describe('When user fills in and submits form', () => {
+//   it('submit method is called with title and content', () => {
+//     const submit = jest.fn()
+//     render(<Register submit={submit} />)
 
-    const title = screen.getByLabelText(/title/i)
-    userEvent.type(title, 'Register')
+//     const title = screen.getByLabelText(/title/i)
+//     userEvent.type(title, 'Register')
 
-    const content = screen.getByLabelText(/content/i)
-    userEvent.type(content, 'Lorem ipsum dolor sit amet')
+//     const content = screen.getByLabelText(/content/i)
+//     userEvent.type(content, 'Lorem ipsum dolor sit amet')
 
-    const button = screen.getByRole('button', { name: /submit/i })
-    userEvent.click(button)
+//     const button = screen.getByRole('button', { name: /submit/i })
+//     userEvent.click(button)
 
-    expect(submit).toHaveBeenCalledWith({
-      title: 'My awesome post',
-      content: 'Lorem ipsum dolor sit amet',
-    })
-  })
-})
+//     expect(submit).toHaveBeenCalledWith({
+//       title: 'My awesome post',
+//       content: 'Lorem ipsum dolor sit amet',
+//     })
+//   })
+// })
