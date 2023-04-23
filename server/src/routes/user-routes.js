85b4b4aa1d2bdca75db4efe4ad13b597736ed1c4
jssatorres <juansataz.dev@gmail.com>
2023-04-23 12:37:28 +0200

    feat (user routes) add routes

--- server/src/routes/user-routes.js
+++ server/src/routes/user-routes.js
@@ -1,25 +1,21 @@
 import { Router } from "express";
 
-// import {
-//   getUsers,
-//   createUser,
-//   updateUser,
-//   deleteUser,
-//   signUp,
-// } from "../controllers/user-controller.js";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  signUp,
+} from "../controllers/user-controller.js";
 
 const UserRouter = Router();
 
-UserRouter.use("/users", (req, res) => {
-  res.status(200).send({
-    data: " users",
-  });
-});
-
-// UserRouter.get("/user/:userId", getUserDetails);
-// UserRouter.post("/users/", createUser);
-// UserRouter.patch("/users/:userId", updateUser);
-// UserRouter.delete("/users/:userId", deleteUser);
+UserRouter.get("/users/:userId", getUser);
+UserRouter.get("/users", getUsers);
+UserRouter.post("/users/", createUser);
+UserRouter.patch("/users/:userId", updateUser);
+UserRouter.delete("/users/:userId", deleteUser);
 // UserRouter.post("/sign-up", authMiddleware, signUp);
 // UserRouter.patch("/user/:userId", updateUser);
 // UserRouter.post("/sign-up",  signUp);
