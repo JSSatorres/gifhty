cf021614c1ab43c7b699e7d8232f22ab0d626715
jssatorres <juansataz.dev@gmail.com>
2023-04-23 12:36:04 +0200

    feat (Server) add cors and white list

--- server/src/server.js
+++ server/src/server.js
@@ -7,13 +7,23 @@ import cors from "cors";
 import userRouter from "./routes/user-routes.js";
 import giftRouter from "./routes/gift-routes.js";
 
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 const app = express();
 
 app.use(express.json())
 app.use(morgan("dev"));
-app.use(helmet());
-app.use(cors());
+// app.use(helmet());
+app.use(cors(corsOptions));
 
 app.use(userRouter);
 app.use(giftRouter);
