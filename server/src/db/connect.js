61626d482e606baadf3ae01f9dd61bf1850c2754
jssatorres <juansataz.dev@gmail.com>
2023-04-25 18:17:51 +0200

    feat (MongoDB) add options

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import { MONGO_URL } from "../config/config.js";
 
+const mongoOpts =  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
 export default async function connectDB (){
   try{
-      const db = await mongoose.connect( MONGO_URL)
-      console.log("conecto to db ", db.connection.name );
+      const db = await mongoose.connect( MONGO_URL,mongoOpts)
+      console.log("concect DB ok to =>", db.connection.name );
   }catch(error){
-      console.error(error);
+      console.log(error);
+      throw new Error ('DB error:' +  error)
   }
 }
