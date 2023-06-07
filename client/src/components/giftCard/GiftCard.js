77a05213a894790d3e511d59624f39be8f14d09d
jssatorres <juansataz.dev@gmail.com>
2023-06-07 07:36:26 +0200

    feat: GiftCard styles animation

--- client/src/components/giftCard/GiftCard.js
+++ client/src/components/giftCard/GiftCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './giftCard.css'
 
 function GiftCard({ title, id, url }) {
   return (
-    <div className="card text-white bg-dark mb-3 col-md-3  px-1 mx-5 border border-warning rounded-5" key={id}>
-      <div className="card-header">{title}</div>
+    <div className="card text-white  mb-3 col-md-3 px-1 mx-5 border border-warning rounded-5 square-in-center-animation background-transition" key={id}>
+      <div className="card-header square-in-center-animation">{title}</div>
       <div className="card-body">
         <img className="rounded mx-auto my-auto d-block img-fluid" src={url} alt={title} />
       </div>
