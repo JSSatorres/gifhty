271acd934a52d9a1db3e8691b098a7929494648e
jssatorres <juansataz.dev@gmail.com>
2023-06-08 21:41:07 +0200

    clean code

--- client/src/components/giftCard/GiftCard.js
+++ client/src/components/giftCard/GiftCard.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './giftCard.css'
 
-function GiftCard({ title, id, url }) {
+function GiftCard({ title, id, url }) { 
+   
   return (
     <div className="card text-white  mb-3 col-md-3 px-1 mx-5 border border-warning rounded-5 square-in-center-animation background-transition" key={id}>
       <div className="card-header square-in-center-animation">{title}</div>
